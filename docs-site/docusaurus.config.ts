ad406cd25ffb6d73ba68e041558cf3b5c106085a
Felipe K. Lisak <felipeklisak@gmail.com>
2026-05-28 10:40:36 -0300

    fix: set baseUrl to root for custom domain deployment

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   },
 
   url: 'https://docs.techgears.app',
-  baseUrl: '/programmable-ecu/',
+  baseUrl: '/',
 
   organizationName: 'TechGearsInteli',
   projectName: 'programmable-ecu',
